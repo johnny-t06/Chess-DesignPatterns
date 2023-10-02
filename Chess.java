9c55ae06a12c0fbfa4fb4d59a4231756f71355b0
Johnny Tan <jonnytan0818@gmail.com>
2023-10-01 22:23:26 -0400

    test case

--- Chess.java
+++ Chess.java
@@ -1,4 +1,28 @@
 public class Chess {
+	public static void test1() {
+		Board b = Board.theBoard();
+		Piece.registerPiece(new BishopFactory());
+		Piece blackBishop = Piece.createPiece("bb");
+		assert blackBishop.toString() = "bb";	//check if createPiece is correct
+		
+		b.addPiece(blackBishop, "c8");
+		b.addPiece(blackBishop, "f8");
+
+		Piece c8 = b.getPiece("c8");			//check if getPiece is correct
+		assert c8.toString() = "bb";
+
+		Piece f8 = b.getPiece("f8");			
+		assert f8.toString() = "bb";
+
+		b.movePiece("c8", "b7");				//check if movePiece is correct
+		Piece b7 = b.getPiece("b7");			
+		assert b7.toString() = "bb";		
+
+		b.movePiece("f8", "g7");				
+		Piece g7 = b.getPiece("g7");			
+		assert g7.toString() = "bb";	
+
+	}
     public static void main(String[] args) {
 	if (args.length != 2) {
 	    System.out.println("Usage: java Chess layout moves");
